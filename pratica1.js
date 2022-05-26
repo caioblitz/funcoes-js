e29040bea9210869647a3667d0689047c6b2fd4e
caiob <caioblitz@gmail.com>
2022-05-26 13:50:38 -0300

    alunos e media

--- pratica1.js
+++ pratica1.js
@@ -1,7 +1,25 @@
+const alunos = [
+	{
+		nome: 'João',
+		nota: 5,
+		turma: '1B',
+	},
+	{
+		nome: 'Sofia',
+		nota: 9,
+		turma: '1B',
+	},
+	{
+		nome: 'Paulo',
+		nota: 6,
+		turma: '2C',
+	},
+];
 function alunosAprovados(arr, media) {
     let aprovados = [];
 
     for(let i = 0; i < arr.length;i++) {
-        
+        if
+     
     }
 }
