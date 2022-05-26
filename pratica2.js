d37c0ce5a1ed019b2ff8bec4bc4c1a1ae5abf815
caiob <caioblitz@gmail.com>
2022-05-26 14:03:40 -0300

    funcao calcula idade

---
+++ pratica2.js
@@ -0,0 +1,25 @@
+function calculaIdade(anos) {
+	return `Daqui a ${anos} anos, ${this.nome} terá ${
+		this.idade + anos
+	} anos de idade.`;
+}
+
+const pessoa1 = {
+    nome: "Caio" ,
+    idade : 30
+};
+
+const pessoa2= {
+    nome: "Thais" ,
+    idade: 24,
+};
+
+const animal = {
+    nome: "Tina", 
+    idade: 6,
+    raca: 'SRD',
+};
+
+console.log(calculaIdade.call(pessoa2, 30))
+console.log(calculaIdade.call(pessoa1, 30))
+console.log(calculaIdade.call(animal, 10))
